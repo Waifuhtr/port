b16dd99e042882df51fd0ce01b2c6e0ba7bf0ba2
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2026-05-13 12:56:20 -0500

    rector: remove specific fonts to use variable types

--- composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/theme/Font.kt
+++ composeApp/src/commonMain/kotlin/com/yoimerdr/compose/ludens/app/theme/Font.kt
@@ -5,29 +5,10 @@ import androidx.compose.runtime.Stable
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import ludens.composeapp.generated.resources.Res
-import ludens.composeapp.generated.resources.inter_black
-import ludens.composeapp.generated.resources.inter_bold
-import ludens.composeapp.generated.resources.inter_italic
-import ludens.composeapp.generated.resources.inter_light
-import ludens.composeapp.generated.resources.inter_medium
-import ludens.composeapp.generated.resources.inter_regular
-import ludens.composeapp.generated.resources.inter_semibold
-import ludens.composeapp.generated.resources.outfit_black
-import ludens.composeapp.generated.resources.outfit_light
-import ludens.composeapp.generated.resources.outfit_medium
-import ludens.composeapp.generated.resources.outfit_regular
-import ludens.composeapp.generated.resources.outfit_semibold
-import ludens.composeapp.generated.resources.plusjakartasans_bold
-import ludens.composeapp.generated.resources.plusjakartasans_italic
-import ludens.composeapp.generated.resources.plusjakartasans_light
-import ludens.composeapp.generated.resources.plusjakartasans_medium
-import ludens.composeapp.generated.resources.plusjakartasans_regular
-import ludens.composeapp.generated.resources.plusjakartasans_semibold
+import ludens.composeapp.generated.resources.inter
 import ludens.composeapp.generated.resources.notosans_sc
+import ludens.composeapp.generated.resources.plusjakartasans
 import org.jetbrains.compose.resources.Font
-
-@Suppress("ObjectPropertyName")
-private var _Outfit: FontFamily? = null
 
 @Suppress("ObjectPropertyName")
 private var _Inter: FontFamily? = null
@@ -36,37 +17,6 @@ private var _Inter: FontFamily? = null
 private var _PlusJakartaSans: FontFamily? = null
 
 
-val Outfit: FontFamily
-    @Stable
-    @Composable
-    get() {
-        if (_Outfit == null) {
-            _Outfit = FontFamily(
-                Font(
-                    Res.font.outfit_light,
-                    weight = FontWeight.Light
-                ),
-                Font(
-                    Res.font.outfit_regular,
-                    weight = FontWeight.Normal
-                ),
-                Font(
-                    Res.font.outfit_medium,
-                    weight = FontWeight.Medium
-                ),
-                Font(
-                    Res.font.outfit_black,
-                    weight = FontWeight.Black
-                ),
-                Font(
-                    Res.font.outfit_semibold,
-                    weight = FontWeight.SemiBold
-                )
-            )
-        }
-        return _Outfit!!
-    }
-
 val Inter: FontFamily
     @Stable
     @Composable
@@ -74,32 +24,28 @@ val Inter: FontFamily
         if (_Inter == null) {
             _Inter = FontFamily(
                 Font(
-                    Res.font.inter_light,
+                    Res.font.inter,
                     weight = FontWeight.Light
                 ),
                 Font(
-                    Res.font.inter_regular,
+                    Res.font.inter,
                     weight = FontWeight.Normal
                 ),
                 Font(
-                    Res.font.inter_medium,
+                    Res.font.inter,
                     weight = FontWeight.Medium
                 ),
                 Font(
-                    Res.font.inter_bold,
+                    Res.font.inter,
                     weight = FontWeight.Bold
                 ),
                 Font(
-                    Res.font.inter_black,
+                    Res.font.inter,
                     weight = FontWeight.Black
                 ),
                 Font(
-                    Res.font.inter_semibold,
+                    Res.font.inter,
                     weight = FontWeight.SemiBold
-                ),
-                Font(
-                    Res.font.inter_italic,
-                    weight = FontWeight.Normal
                 ),
                 Font(
                     Res.font.notosans_sc,
@@ -124,7 +70,7 @@ val Inter: FontFamily
                 Font(
                     Res.font.notosans_sc,
                     weight = FontWeight.Black
-                )
+                ),
             )
         }
 
@@ -138,28 +84,24 @@ val PlusJakartaSans: FontFamily
         if (_PlusJakartaSans == null) {
             _PlusJakartaSans = FontFamily(
                 Font(
-                    Res.font.plusjakartasans_light,
+                    Res.font.plusjakartasans,
                     weight = FontWeight.Light
                 ),
                 Font(
-                    Res.font.plusjakartasans_regular,
+                    Res.font.plusjakartasans,
                     weight = FontWeight.Normal
                 ),
                 Font(
-                    Res.font.plusjakartasans_medium,
+                    Res.font.plusjakartasans,
                     weight = FontWeight.Medium
                 ),
                 Font(
-                    Res.font.plusjakartasans_bold,
+                    Res.font.plusjakartasans,
                     weight = FontWeight.Bold
                 ),
                 Font(
-                    Res.font.plusjakartasans_semibold,
+                    Res.font.plusjakartasans,
                     weight = FontWeight.SemiBold
-                ),
-                Font(
-                    Res.font.plusjakartasans_italic,
-                    weight = FontWeight.Normal
                 ),
                 Font(
                     Res.font.notosans_sc,
